fdecf861afc87de063904ba72d40bc0b43cb84a2
Ryan A. Anderson <ryan.anderson9921@stu.mtec.edu>
2022-01-11 07:29:52 -0700

    Initial Commit

---
+++ RyanCard/RyanCardApp.swift
@@ -0,0 +1,17 @@
+//
+//  RyanCardApp.swift
+//  RyanCard
+//
+//  Created by Ryan Anderson on 1/11/22.
+//
+
+import SwiftUI
+
+@main
+struct RyanCardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
